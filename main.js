663577ffbd50ef03e7109f020a83df05ed958c25
drmadiha <drmadiha1may@gmail.com>
2024-04-02 09:05:14 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+const randomNumber = 13;
+const answers = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number",
+    }
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congtratulation you guessed right number");
+}
+else {
+    console.log("you guessed wrong number");
+}
